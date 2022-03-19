1df7135d60a453e58d0b74c5d6480efdc7c598fb
GustavoArcoverde <garcoverde3@gmail.com>
2022-03-19 15:32:58 -0300

    add logica de escolha em js na fase 3 da personagem melgar

---
+++ História Melgar/fase 3/melgar-fase3.js
@@ -0,0 +1,18 @@
+function resposta() {
+    var resposta = prompt(`1. Enfrentá-los!
+2. Se esconder embaixo da mesa.`)
+    var contador = 1
+    var tentativas = 10
+
+    while(tentativas > contador){
+
+        if(resposta == "2" || resposta == "2. Direita") {
+        window.location.href = "../fase 4/melgar-fase4.html";
+        break
+
+        }
+        alert("Você toma a iniciativa de enfrentá-los. Mas, você não dura muito contra as flechas dos assassinos.")
+        resposta = prompt("Tente novamente.")
+        contador++
+}
+}
