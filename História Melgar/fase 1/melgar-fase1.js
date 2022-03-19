293b07291c7713e7c1f21de4ba34f57d16a300ba
GustavoArcoverde <garcoverde3@gmail.com>
2022-03-19 15:22:42 -0300

    add logica de escolha com js na fase 1 da personagem melgor

---
+++ História Melgar/fase 1/melgar-fase1.js
@@ -0,0 +1,18 @@
+        function resposta() {
+            var resposta = prompt(`1. Ir atrás desse traste! 
+2. Ficar bebendo e comprar outro alaúde depois.`)
+            var contador = 1
+            var tentativas = 10
+
+            while(tentativas > contador){
+
+                if(resposta == "1" || resposta == "1. Ir atrás desse traste!") {
+                window.location.href = "../fase 2/melgar-fase2.html";
+                break
+
+                }
+                alert("Você permanece bebendo e aproveitando a noite, depois de um tempo, quando a bebida faz efeito, você esquece completamente do homem encapuzado que havia lhe tirado do sério. No dia seguinte, após uma longa ressaca, você vai até o comerciante mais próximo a procura de outro alaúde. Assim o faz, mas fica com um gostinho de “não é a mesma coisa” na boca, e se pergunta: “O que será que aquele miserável fez com meu precioso alaúde?”. Pergunta essa que fica sem resposta...")
+                resposta = prompt("Tente novamente.")
+                contador++
+        }
+        }
