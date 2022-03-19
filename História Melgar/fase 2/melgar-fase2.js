bfff18aa369f39225c80fa5b4a173c8be590e99f
GustavoArcoverde <garcoverde3@gmail.com>
2022-03-19 15:28:38 -0300

    add logica de escolha em js na fase 2 da personagem melgar

---
+++ História Melgar/fase 2/melgar-fase2.js
@@ -0,0 +1,18 @@
+function resposta() {
+    var resposta = prompt(`1. Esquerda.
+2. Direita.`)
+    var contador = 1
+    var tentativas = 10
+
+    while(tentativas > contador){
+
+        if(resposta == "2" || resposta == "2. Direita") {
+        window.location.href = "../fase 3/melgar-fase3.html";
+        break
+
+        }
+        alert("Você chega a uma sala completamente vazia, estranhamente limpa, mas vazia. De repente, a porta se fecha e tranca quando você entra e um gás começa a ser expelido pelos cantos da sala. Em minutos, você já não consegue mais respirar.")
+        resposta = prompt("Tente novamente.")
+        contador++
+}
+}
